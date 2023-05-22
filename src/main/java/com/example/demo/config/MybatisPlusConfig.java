67c103c7ce9c6914a6005735b2dc53fe0e64edd0
Xu-yyyyya <1198823730@qq.com>
2023-05-22 21:35:16 +0800

    first commmit

---
+++ src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration//所有配置想注入springboot容器都要加这个注解
+@MapperScan("com.example.demo.mapper")//把mapper的所有接口注入到容器里
+public class MybatisPlusConfig {
+    // 最新版
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
